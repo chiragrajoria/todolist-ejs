18db8d0c8b5bb428ea526fee31d19b17c49013aa
chiragrajoria <chiragrajoria05@gmail.com>
2022-03-20 22:14:59 +0530

    update

--- app.js
+++ app.js
@@ -129,8 +129,12 @@ app.get("/work", function (req, res) {
     res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log("Server started on port 3000");
 })
